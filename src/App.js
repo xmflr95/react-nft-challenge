b304157a1a3b15f881354e25a25a01c7ac9aa7b2
ChangHui Han <xmflr95@naver.com>
2022-01-27 17:20:06 +0900

    react nft challenge first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import CollectionCard from './components/CollectionCard';
+import Header from './components/Header';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Punklist from './components/Punklist';
+import Main from './components/Main';
 
 function App() {
+  const [punkListData, setPunkListData] = useState([])
+  const [selectedPunk, setSelectedPunk] = useState(0)
+
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        'https://testnets-api.opensea.io/assets?asset_contract_address=0x2842a268AE06FDEE46607f7b6501D282a3458b37&order_direction=asc'
+      )
+      console.log(openseaData.data.assets)
+      setPunkListData(openseaData.data.assets)
+    }
+
+    return getMyNfts()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <Punklist 
+            punkListData={punkListData} 
+            setSelectedPunk={setSelectedPunk} 
+          />
+        </>
+      )}      
     </div>
   );
 }
